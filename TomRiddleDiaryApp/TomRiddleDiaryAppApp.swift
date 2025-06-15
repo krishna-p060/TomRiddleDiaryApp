43661634e871d9d561f836f4c91f49f7f491f2cd
Krishna Patidar <113329860+krishna-p060@users.noreply.github.com>
2025-06-16 00:11:15 +0800

    Initial Commit

---
+++ TomRiddleDiaryApp/TomRiddleDiaryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TomRiddleDiaryAppApp.swift
+//  TomRiddleDiaryApp
+//
+//  Created by Apple on 16/06/25.
+//
+
+import SwiftUI
+
+@main
+struct TomRiddleDiaryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
